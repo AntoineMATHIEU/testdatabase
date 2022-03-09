c85cca1cb451fed8b091d03a976bdcaa93ce489a
Antoine Mathieu <antoine04.mathieu@gmail.com>
2022-03-09 10:33:34 +0100

    changer void en int pour selectGold

--- src/Update.java
+++ src/Update.java
@@ -4,34 +4,12 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class Update {
-    /**
-     * Connect to the test.db database
-     *
-     * @return the Connection object
-     */
-    private Connection connect() {
-        // SQLite connection string
-        String url = "jdbc:sqlite:C:\\Users\\tng13\\Desktop\\testdatabase\\Bombermantest.db";
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(url);
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return conn;
-    }
 
-    /**
-     * Update le niveau d'un joueur
-     *
-     *@param username nom du joeur
-     *@param level niveau du joueur
-     */
     public void updateLevel(String username, int level) {
         String sql = "UPDATE bomberman SET level = ? "
                 + "WHERE username= ?";
 
-        try (Connection conn = this.connect();
+        try (Connection conn = connect.connect();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
             // set the corresponding param
@@ -48,7 +26,7 @@ public class Update {
         String sql = "UPDATE bomberman SET gold = ? "
                 + "WHERE username= ?";
 
-        try (Connection conn = this.connect();
+        try (Connection conn = connect.connect();
              PreparedStatement pstmt = conn.prepareStatement(sql)) {
 
             // set the corresponding param
